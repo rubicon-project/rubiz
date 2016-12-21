cccb7cf8197a75afa282ff3cc2f4a714819a4d31
Colt Frederickson <coltfred@gmail.com>
2016-12-20 17:33:02 -0700

    Cover the uncovered syntax

--- src/test/scala/rubiz/syntax/EitherSyntaxTest.scala
+++ src/test/scala/rubiz/syntax/EitherSyntaxTest.scala
@@ -6,14 +6,22 @@ import scalaz.\/
 import scalaz.effect.IO
 
 class EitherSyntaxTest extends rubiz.WordSpecBase {
+  val right = "foo".right[Throwable]
+  val left = \/.fromTryCatchNonFatal[String](throw new Exception("broken"))
   "EitherSyntax.toM" should {
-    val right = "foo".right[Throwable]
-    val left = \/.fromTryCatchNonFatal[String](throw new Exception("broken"))
     "put exception into fail" in {
       left.toM[IO].catchLeft.unsafePerformIO.leftValue shouldBe an[Exception]
     }
     "put a into M" in {
       right.toM[IO].catchLeft.unsafePerformIO.value shouldBe "foo"
+    }
+  }
+  "EitherSyntax.toTask" should {
+    "convert exception to fail" in {
+      left.toTask.attemptRun shouldBe 'left
+    }
+    "convert right to fail" in {
+      right.toTask.attemptRun shouldBe 'right
     }
   }
 }
--- src/test/scala/rubiz/syntax/TrySyntaxTest.scala
+++ src/test/scala/rubiz/syntax/TrySyntaxTest.scala
@@ -6,10 +6,19 @@ import all._
 class TrySyntaxTest extends rubiz.WordSpecBase {
   "Try.toDisjunction" should {
     "create left for Failure" in {
-      Try(throw new Exception()).toDisjunction shouldBe (left)
+      Try(throw new Exception()).toDisjunction shouldBe 'left
     }
     "create right for values" in {
       Try(1).toDisjunction.value shouldBe 1
+    }
+  }
+
+  "Try.toTask" should {
+    "create Task.fail for Failure" in {
+      Try(throw new Exception()).toTask.attemptRun shouldBe 'left
+    }
+    "create Task.now for values" in {
+      Try(1).toTask.attemptRun.value shouldBe 1
     }
   }
 }
